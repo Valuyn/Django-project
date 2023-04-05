e7ce096cfdfe41c05266f43934e94e3fd14bc27c
Valuyn <f.kostya.o@gmail.com>
2023-04-05 12:14:26 +0300

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,7 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField(max_length=15)
+    description = models.Charfield(max_length=100)
 
     def __str__(self):
         return self.title
